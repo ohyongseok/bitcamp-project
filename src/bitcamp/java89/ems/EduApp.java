63bac046caa690dc5630f9faa0001699727bf72e
ohyongseok <oys124@gmail.com>
2016-10-28 14:01:37 +0900

    .

--- src/bitcamp/java89/ems/EduApp.java
+++ src/bitcamp/java89/ems/EduApp.java
@@ -6,21 +6,18 @@ public class EduApp {
   static Scanner keyScan = new Scanner(System.in);
 
   public static void main(String[] args) {
-    StudentController studentController = new StudentController(keyScan);
+    TeacherController teacherController = new TeacherController(keyScan);
 
     System.out.println("비트캠프 관리시스템에 오신걸 환영합니다.");
 
     loop:
-    while (true) {
+      while (true) {
       System.out.print("명령> ");
       String command = keyScan.nextLine().toLowerCase();
 
       switch (command) {
-      case "add": studentController.doAdd(); break;
-      case "list": studentController.doList(); break;
-      case "view": studentController.doView(); break;
-      case "delete": studentController.doDelete(); break;
-      case "update": studentController.doUpdate(); break;
+      case "menu": doMenu(); break;
+      case "go 1": teacherController.service(); break;
       case "quit":
         System.out.println("Good bye!");
         break loop;
@@ -28,6 +25,11 @@ public class EduApp {
         System.out.println("지원하지 않는 명령어입니다.");
       }
     }
+  }
+  public static void doMenu() {
+    System.out.println("[메뉴]");
+    System.out.println("1.학생관리");
+    System.out.println("메뉴 이동은 'go 메뉴번호(EX> go 1)'를 입력하세요");
   }
 }
 
